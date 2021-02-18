f9608dbf3c83252f66ba9a0a32459691d4191ec5
Thomasit99 <thomasverhage@outlook.com>
2021-02-18 11:13:18 +0100

    Updated DB

--- apotheek.sql
+++ apotheek.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 16 feb 2021 om 13:56
+-- Gegenereerd op: 18 feb 2021 om 11:12
 -- Serverversie: 10.4.17-MariaDB
--- PHP-versie: 8.0.1
+-- PHP-versie: 8.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,10 +42,52 @@ CREATE TABLE `login` (
 -- Gegevens worden geëxporteerd voor tabel `login`
 --
 
-    INSERT INTO `login` (`id`, `username`, `password`, `email`, `activation_code`, `activation_status`, `forgot_password_code`, `forgot_password_time`) VALUES
-    (7, 'haha', 'hihi', 'hoho', '', 0, '', '0000-00-00 00:00:00.000000'),
-    (37, 'ItzDylan', '$2y$10$GXG6kpOHTKhtUYlZLHIfv.7mcQFLWUpoMUxlPOWJN9Hh./93fxpPu', 'dylan-dylan99@hotmail.com', '7fe24e6a9870ba2a4eb6731c26bd416c', 1, '', '0000-00-00 00:00:00.000000'),
-    (46, 'qwerty', '$2y$10$kqz/26Gc4KN4/itBYgcAyuBkLICoyZlKQZNxDSoymIyoCJk2euT8m', 'dylan99@quicknet.nl', '', 1, '', '0000-00-00 00:00:00.000000');
+INSERT INTO `login` (`id`, `username`, `password`, `email`, `activation_code`, `activation_status`, `forgot_password_code`, `forgot_password_time`) VALUES
+(7, 'haha', 'hihi', 'hoho', '', 0, '', '0000-00-00 00:00:00.000000'),
+(37, 'ItzDylan', '$2y$10$GXG6kpOHTKhtUYlZLHIfv.7mcQFLWUpoMUxlPOWJN9Hh./93fxpPu', 'dylan-dylan99@hotmail.com', '7fe24e6a9870ba2a4eb6731c26bd416c', 1, '', '0000-00-00 00:00:00.000000'),
+(46, 'qwerty', '$2y$10$kqz/26Gc4KN4/itBYgcAyuBkLICoyZlKQZNxDSoymIyoCJk2euT8m', 'dylan99@quicknet.nl', '', 1, '', '0000-00-00 00:00:00.000000');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `medicines`
+--
+
+CREATE TABLE `medicines` (
+  `ID` int(11) NOT NULL,
+  `MedicineName` varchar(50) NOT NULL,
+  `MedicineDes` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `medicines`
+--
+
+INSERT INTO `medicines` (`ID`, `MedicineName`, `MedicineDes`) VALUES
+(0, 'Paracetemol', 'Pijnstiller.'),
+(0, 'Melathonine', 'Lekker slapen.'),
+(0, 'Ritalin', 'ADHD behandeling.'),
+(0, 'THC', 'Anti depressie.');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `news`
+--
+
+CREATE TABLE `news` (
+  `ID` int(11) NOT NULL,
+  `NewsName` varchar(50) NOT NULL,
+  `NewsDes` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `news`
+--
+
+INSERT INTO `news` (`ID`, `NewsName`, `NewsDes`) VALUES
+(1, 'Onze website is vernieuwd!', '<a href=\"nieuwe-site.php\">Link</a>'),
+(2, 'Wij gaan bijna onze deuren openen.', '<a href=\"nieuwe.php\">Link</a>');
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -54,8 +96,14 @@ CREATE TABLE `login` (
 --
 -- Indexen voor tabel `login`
 --
-  ALTER TABLE `login`
-    ADD PRIMARY KEY (`id`);
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`ID`);
 
 --
 -- AUTO_INCREMENT voor geëxporteerde tabellen
@@ -64,9 +112,15 @@ CREATE TABLE `login` (
 --
 -- AUTO_INCREMENT voor een tabel `login`
 --
-  ALTER TABLE `login`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
-  COMMIT;
+ALTER TABLE `login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+
+--
+-- AUTO_INCREMENT voor een tabel `news`
+--
+ALTER TABLE `news`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
