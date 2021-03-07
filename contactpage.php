307011977bfee7bd4e2139e684962b004c87ad3a
ItzDylan <dylan-dylan99@hotmail.com>
2021-03-07 16:28:49 +0100

    Contact pagina VERSIE 1

--- contactpage.php
+++ contactpage.php
@@ -1,4 +1,16 @@
 <?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true)
+{
+    echo "You're logged in! Welcome " .$_SESSION["username"];
+}
+else
+{
+	echo "You're not logged in!";
+}
 //Connection file
 require_once "config.php";
 
@@ -21,7 +33,131 @@ $lastname_err = "";
 $message = "";
 $message_err = "";
 
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    // Validate firstname
+    if(empty(trim($_POST["firstname"])))
+    {
+        $firstname_err = "Please enter your first name.";
+    } 
+    else
+    {
+        $firstname = trim($_POST["firstname"]);
+    }
+
+    // Validate lastname
+    if(empty(trim($_POST["lastname"])))
+    {
+        $lastname_err = "Please enter your last name.";
+    } 
+    else
+    {
+        $lastname = trim($_POST["lastname"]);
+    }
+
+    //Validate email
+	if(empty(trim($_POST["email"])))
+	{
+		$email_err = "Please enter an E-Mail address";
+	}
+	else
+	{
+		$email_v = trim($_POST["email"]);
+		if(!filter_var($email_v, FILTER_VALIDATE_EMAIL))
+		{
+			$email_err = "This E-Mail address is invalid!";
+		}
+		else
+		{
+            $email = trim($_POST["email"]);   
+		}
+    }
+
+    // Validate message
+    if(empty(trim($_POST["message"])))
+    {
+        $message_err = "Please enter a message.";
+    } 
+    elseif(strlen(trim($_POST["message"])) < 25)
+    {
+        $message_err = "Your message must consist of atleast 25 characters (max 300).";
+    }
+    elseif(strlen(trim($_POST["message"])) > 300)
+    {
+        $message_err = "This message has exceeded the maximum character length of 300";
+    }
+    else
+    {
+        $message = trim($_POST["message"]);
+    }
+
+    // Check input errors before inserting in database
+    if(empty($firstname_err) && empty($lastname_err) && empty($email_err) && empty($message_err))
+    {
+        // Prepare an insert statement
+        $sql = "INSERT INTO contact (voornaam, achternaam, email, bericht) VALUES (?, ?, ?, ?)";
+         
+        if($stmt = mysqli_prepare($conn, $sql))
+        {
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ssss", $param_firstname, $param_lastname, $param_email, $param_message);
+            
+            // Set parameters
+            $param_firstname = $firstname;
+			$param_email = $email;
+			$param_lastname = $lastname;
+            $param_message = $message;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt))
+			{
+                // Send verification E-Mail
+				try 
+				{
+					//Server settings
+					$mail->SMTPDebug = 0;                      // Enable verbose debug output
+					$mail->isSMTP();                                            // Send using SMTP
+					$mail->Host       = 'SMTP.office365.com';                    // Set the SMTP server to send through
+					$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+					$mail->Username   = 'dylan-dylan99@hotmail.com';                     // SMTP username
+					$mail->Password   = '#Slacht99';                               // SMTP password
+					$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+					$mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+					//Recipients
+					$mail->setFrom('dylan-dylan99@hotmail.com', 'Dylan');
+					$mail->addAddress($_POST["email"]);     // Add a recipient        // Name is optional
+
+					// Content
+					$mail->isHTML(true);                                  // Set email format to HTML
+					$mail->Subject = 'Contact bericht';
+					$mail->Body    = "Bedankt voor uw contact bericht. Uw vraag is bij ons binnen gekomen en zal spoedig beantwoord worden. Houd uw email in de gaten!";
+					// Send mail
+					$mail->send();
+					echo 'Message has been sent';
+				} 
+				catch (Exception $e) 
+				{
+					echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                }
+				// Redirect to login page
+                header("location: mijn-appo.php");
+            } 
+			else
+			{
+                echo "Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    // Close connection
+    mysqli_close($conn);
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -69,7 +205,7 @@ $message_err = "";
                 <input type="submit" class="btn btn-primary" value="Submit">
                 <input type="reset" class="btn btn-default" value="Reset">
             </div>
-            <p>Already have an account? <a href="mijn-appo.php">Login here</a>.</p>
+            <p>Homepagina: <a href="index.php">Klik hier!</a></p>
         </form>
     </div> 
 <?php include 'assets/includes/footer.php'?>	
