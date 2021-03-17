3491dbab95d1ad6d57b171fd77e2c84f98594f53
brandinos <brandon20899@pm.me>
2021-03-17 16:19:10 +0100

    Eerste slideshow

--- index.php
+++ index.php
@@ -114,6 +114,29 @@ else
                 </div>
             </section>
         </main>
+        <script>
+            var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+            </script>
         <?php include 'assets/includes/footer.php'?>
     </body>
 </html>
