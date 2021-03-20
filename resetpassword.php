108a38bd7417ca386aaf054041a97023d1c75695
brandinos <brandon20899@pm.me>
2021-03-20 12:39:44 +0100

    HTML head als PHP include

--- resetpassword.php
+++ resetpassword.php
@@ -120,22 +120,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 ?>
 <!DOCTYPE html>
 <html lang="nl">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link
-            rel="preload"
-            href="https://necolas.github.io/normalize.css/8.0.1/normalize.css"
-            as="style"
-        />
-        <link
-            rel="stylesheet"
-            href="https://necolas.github.io/normalize.css/8.0.1/normalize.css"
-        />
-        <link rel="preload" href="./assets/css/style.css" as="style" />
-        <link rel="stylesheet" href="./assets/css/style.css" />
-        <title>Apotheek Schut</title>
-    </head>
+<?php include "assets/includes/head.php" ?>
 	<body>
 	<main class="wrap">
             <section>
