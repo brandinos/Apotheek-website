5676ed7251880e99c646237b2031fa130e5dc1ac
Dylan Worseling <dylan-dylan99@hotmail.com>
2021-03-25 14:27:37 +0100

    Laaste cleanup #1

--- contactpageguest.php
+++ contactpageguest.php
@@ -108,7 +108,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 					$mail->Host       = 'SMTP.office365.com';                    // Set the SMTP server to send through
 					$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
 					$mail->Username   = 'dylan-dylan99@hotmail.com';                     // SMTP username
-					$mail->Password   = '#Slacht99';                               // SMTP password
+					$mail->Password   = '';                               // SMTP password
 					$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 					$mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
